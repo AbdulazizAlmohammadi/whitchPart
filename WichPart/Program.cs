133763cf28f21c050a4d78827e566f8a00e8c5fd
Abdulaziz Almohammadi <AbdulazizAlmohammadi@users.noreply.github.com>
2021-04-19 03:55:13 +0300

    initial commit

---
+++ WichPart/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WichPart
+{
+    class Program
+    {
+        static string wichPart(String source)
+        {
+            string result="";
+            string am = "ABCDEFGHIGKLMabcdefghigklm";
+            string nz = "NOPQRSTUVWXYZnopqrstuvwxyz";
+
+            foreach (var item in source)
+                if (am.Contains(item)) result += "o";
+                else if (nz.Contains(item)) result += "t";
+                else result += ".";
+            {
+
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(wichPart("abszzzXXCCDDDaaAAEErrEp>KHTEdfldkmf_"));
+        }
+    }
+}
